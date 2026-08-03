e293cf7a28c215ba6d3d92f055bb386c84446e5d
AyushPandey2007 <ayushpandey9024@gmail.com>
2026-08-03 17:02:20 +0530

    Add files via upload

---
+++ Input.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    cout<<"Enter your age: \n";
+    cin>>age;
+    cout<<"Your age is "<<age;
+    return 0;
+}
---
+++ Logical_Operators.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<(true && false)<<endl;//false
+    cout<<(true || false)<<endl;//true
+    cout<<(!true)<<endl;//false
+    return 0;
+}
