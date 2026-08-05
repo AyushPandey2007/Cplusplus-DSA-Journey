eba97d4861b6e0cb92a5180fc97dc48512d63ad2
Ayush Pandey <ayushpandey9024@gmail.com>
2026-08-05 19:38:31 +0530

    Pattern Printing.

---
+++ Lecture-4_Patterns/03_In_the_form_of_character.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter N: ";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        char ch='A';
+        for(int j=0;j<n;j++){
+            cout<<ch<<" ";
+            ch++;
+        } cout<<endl;
+    } return 0;
+}
